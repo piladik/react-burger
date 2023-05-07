cca0f88df17e00e7657a406bb2313113971f9ab3
Chistiakov Vladislav <chistiakov.vladislav@gmail.com>
2023-05-08 00:02:33 +1000

    Добавил скролбар к компоненту FeedOrders. FeedOrder теперь создается при помощи Array.map, а не в ручную. Добавил типизацию пропсов для FeedOrder.

--- src/components/feed-order/feed-order.tsx
+++ src/components/feed-order/feed-order.tsx
@@ -1,43 +1,43 @@
 import styles from "./feed-order.module.css";
 import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 
-function FeedOrder(): JSX.Element {
+interface IFeedOrderProps {
+  name: string;
+  orderId: string;
+  imgs: Array<string>;
+  timestamp: string;
+  price: number;
+}
+
+function FeedOrder({ order }: { order: IFeedOrderProps }): JSX.Element {
+  const { name, orderId, imgs, timestamp, price } = order;
   return (
-    <div className={`mb-6 ${styles.feed_order_card}`}>
+    <div className={`mb-6 mr-2 ${styles.feed_order_card}`}>
       <div className="mr-6 ml-6 card_content">
         <div className={`pt-6 ${styles.card_header}`}>
-          <p className="order_id text text_type_digits-default">#3303030</p>
+          <p className="order_id text text_type_digits-default">{orderId}</p>
           <p className="order_date text text_type_main-default text_color_inactive">
-            Сегодня, 16:20
+            {timestamp}
           </p>
         </div>
-        <p className="text text_type_main-medium pt-6">My Burger</p>
+        <p className="text text_type_main-medium pt-6">{name}</p>
         <div className={`${styles.card_footer}`}>
           <div className={`pt-6 pb-6 mr-6 ${styles.ingredients_img_box}`}>
-            <div className={`${styles.img_wrapper}`} style={{ zIndex: 3 }}>
-              <img
-                src="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
-                alt="test"
-                className={`${styles.ingredients_img}`}
-              />
-            </div>
-            <div className={`${styles.img_wrapper}`} style={{ zIndex: 2 }}>
-              <img
-                src="https://code.s3.yandex.net/react/code/meat-01-mobile.png"
-                alt="test"
-                className={`${styles.ingredients_img}`}
-              />
-            </div>
-            <div className={`${styles.img_wrapper}`} style={{ zIndex: 1 }}>
-              <img
-                src="https://code.s3.yandex.net/react/code/sauce-02-mobile.png"
-                alt="test"
-                className={`${styles.ingredients_img}`}
-              />
-            </div>
+            {imgs.map((img, index) => (
+              <div
+                className={`${styles.img_wrapper}`}
+                style={{ zIndex: 100 - index }}
+              >
+                <img
+                  src={img}
+                  alt="test"
+                  className={`${styles.ingredients_img}`}
+                />
+              </div>
+            ))}
           </div>
           <div className={`${styles.total_box}`}>
-            <p className="order_total text text_type_digits-default">1488</p>
+            <p className="order_total text text_type_digits-default">{price}</p>
             <CurrencyIcon type="primary" />
           </div>
         </div>
--- src/components/feed-orders/feed-orders.tsx
+++ src/components/feed-orders/feed-orders.tsx
@@ -1,12 +1,58 @@
 import styles from "./feed-orders.module.css";
 import { FeedOrder } from "../feed-order/feed-order";
 
+export interface ITestData {
+  name: string;
+  orderId: string;
+  imgs: Array<string>;
+  timestamp: string;
+  price: number;
+}
+const testData: ITestData[] = [
+  {
+    name: "My Burger",
+    orderId: "#3303030",
+    imgs: [
+      "https://code.s3.yandex.net/react/code/bun-02-mobile.png",
+      "https://code.s3.yandex.net/react/code/meat-01-mobile.png",
+      "https://code.s3.yandex.net/react/code/sauce-02-mobile.png",
+    ],
+    timestamp: "Сегодня, 16:20",
+    price: 2333,
+  },
+  {
+    name: "My Burger",
+    orderId: "#3303030",
+    imgs: [
+      "https://code.s3.yandex.net/react/code/bun-02-mobile.png",
+      "https://code.s3.yandex.net/react/code/meat-01-mobile.png",
+      "https://code.s3.yandex.net/react/code/sauce-02-mobile.png",
+    ],
+    timestamp: "Сегодня, 17:30",
+    price: 1606,
+  },
+  {
+    name: "My Burger",
+    orderId: "#3303030",
+    imgs: [
+      "https://code.s3.yandex.net/react/code/bun-02-mobile.png",
+      "https://code.s3.yandex.net/react/code/meat-01-mobile.png",
+      "https://code.s3.yandex.net/react/code/sauce-02-mobile.png",
+    ],
+    timestamp: "Сегодня, 18:59",
+    price: 1613,
+  },
+];
+
 function FeedOrders(): JSX.Element {
   return (
     <section className={`mt-10 ${styles.feed_orders_section}`}>
       <h1 className="text text_type_main-large mb-6">Лента заказов</h1>
-      <FeedOrder />
-      <FeedOrder />
+      <div className={`${styles.scrollable_box}`}>
+        {testData.map((el, index) => (
+          <FeedOrder order={el} key={index} />
+        ))}
+      </div>
     </section>
   );
 }
