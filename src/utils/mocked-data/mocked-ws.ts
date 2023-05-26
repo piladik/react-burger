7df049368ffe0857d8cd8c4a819c59e8b00e1514
Chistiakov Vladislav <chistiakov.vladislav@gmail.com>
2023-05-26 15:39:11 +1000

    Добавил тесты для reducers/ws-feed & ws-profile

---
+++ src/utils/mocked-data/mocked-ws.ts
@@ -0,0 +1,15 @@
+export const mockedWsFeedOrders = [
+  {
+    _id: "647030a18a4b62001c83f1ee",
+    ingredients: [
+      "643d69a5c3f7b9001cfa093d",
+      "643d69a5c3f7b9001cfa093e",
+      "643d69a5c3f7b9001cfa093d",
+    ],
+    status: "done",
+    name: "Люминесцентный флюоресцентный бургер",
+    createdAt: "2023-05-26T04:08:01.148Z",
+    updatedAt: "2023-05-26T04:08:01.264Z",
+    number: 5416,
+  },
+];
