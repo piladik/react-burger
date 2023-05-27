56d399b9869d30284f19b5605fa729a54d348715
Chistiakov Vladislav <chistiakov.vladislav@gmail.com>
2023-05-27 20:06:39 +1000

    Deploy gh pages init

--- src/index.tsx
+++ src/index.tsx
@@ -4,20 +4,20 @@ import "./index.css";
 import App from "./components/app/app";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import store from "./services/slices";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <React.StrictMode>
       <Provider store={store}>
         <App />
       </Provider>
     </React.StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
